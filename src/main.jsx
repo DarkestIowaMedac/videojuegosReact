5eca5aa25d923248330be04b433f2ebc1a0bf82e
DarkestIowaMedac <153643708+DarkestIowaMedac@users.noreply.github.com>
2025-02-12 23:19:21 +0100

    Redefinidas listas de tareas

--- src/main.jsx
+++ src/main.jsx
@@ -15,10 +15,22 @@ root.render(
 /*
 TAREAS POR HACER:
 
--Plantear funcionalidad barra de busqueda
+
 -Incorporar Logo
--Separación de las llamadas a la API en api.js
--Hacer cambio de página en juegos recientes
--Mejorar estilos en la página de lista de juegos (opcional)
+-Hacer paginación en juegos recientes (Opcional)
+-Mejorar estilos en la página de detalle de juego (Opcional)
+-Mejorar estilos en la página de lista de juegos (Opcional)
 -Hacer filtros
+-Hacer favoritos
+En la página de detalles de un videojuego deben mostrarse como mínimo:
+○ Tags
+○ Publisher
+○ Plataformas
+● Cada tag/género debe ser clickable y debe llevar a una nueva página que muestre
+los juegos de un tag/género
+● Cada publisher será un enlace que llevará una página con información sobre el
+publisher y sus videojuegos
+● Añade paginación a la página de lista de videojuegos. (HECHO)
+● Crea una nueva página similar a la principal de videojuegos, pero donde se
+buscarán Publishers
 */
